cfc9357468d6e5f3c76c21caddff340340582ba4
bhy <450215151@qq.com>
2019-07-26 10:16:13 +0800

    d
    d

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const fs = require('fs-extra')
 const path = require('path')
 const _ = require('loadsh')
-fs.writeFile(path.join(__dirname, '../dd.txt') , _.random(1, 100))
+fs.writeFile(path.join(__dirname, '../ssss.txt') , _.random(1, 100))
