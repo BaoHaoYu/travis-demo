435e39983f3c2f9c7618f7888464200305476954
bhy <450215151@qq.com>
2019-10-22 18:33:29 +0800

    demo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ const fs = require("fs-extra");
 const path = require("path");
 const _ = require("loadsh");
 const shelljs = require("shelljs");
-
+const child_process = require("child_process");
+child_process.execSync(
+  'git log -1 --pretty=format:"%cD" ' + path.join(__dirname, "./src")
+);
+child_process.spawnSync(
+  'git log -1 --pretty=format:"%cD" ' + path.join(__dirname, "./src")
+);
 shelljs.exec(
   'git log -1 --pretty=format:"%cD" ' + path.join(__dirname, "./src")
 );
