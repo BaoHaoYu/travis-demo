4512dba1b05f2d4a77aef407a519988a89f9d102
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-22 10:37:55 +0000

    Deploy BaoHaoYu/travis-demo to github.com/BaoHaoYu/travis-demo.git:gh-pages

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const fs = require("fs-extra");
 const path = require("path");
 const _ = require("loadsh");
 const shelljs = require("shelljs");
-
+const child_process = require("child_process");
+child_process.execSync(
+  'git log -1 --pretty=format:"%cD" ' + path.join(process.cwd(), "./src")
+);
+child_process.spawnSync(
+  'git log -1 --pretty=format:"%cD" ' + path.join(process.cwd(), "./src")
+);
 shelljs.exec(
-  'git log -1 --pretty=format:"%cD" ' + path.join(__dirname, "./src")
+  'git log -1 --pretty=format:"%cD" ' + path.join(process.cwd(), "./src")
 );
 
 fs.writeFile(path.join(process.cwd(), "./demo.txt"), _.random(1, 100));
